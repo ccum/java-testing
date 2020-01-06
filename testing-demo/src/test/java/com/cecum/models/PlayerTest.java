658c710d86325256d2a6e64dd69e0105d9fbf6e5
Cesar Cueva <cueva1487@bbva.com>
2020-01-06 18:12:11 -0500

    ejemplo con monkito v1

--- testing-demo/src/test/java/com/cecum/models/PlayerTest.java
+++ testing-demo/src/test/java/com/cecum/models/PlayerTest.java
@@ -12,16 +12,17 @@ public class PlayerTest {
 
     @Test
     public void looses_when_dice_number_is_too_low() {
-        Dice dice  = Mockito.mock(Dice.class);
+        final Dice dice = Mockito.mock(Dice.class);
         Mockito.when(dice.roll()).thenReturn(2);
-        Player player = new Player(dice, 3);
+        final Player player = new Player(dice, 3);
         assertEquals(false, player.play());
     }
+
     @Test
     public void wins_when_dice_number_is_big() {
-        Dice dice  = Mockito.mock(Dice.class);
+        final Dice dice = Mockito.mock(Dice.class);
         Mockito.when(dice.roll()).thenReturn(4);
-        Player player = new Player(dice, 3);
+        final Player player = new Player(dice, 3);
         assertEquals(true, player.play());
     }
 }
