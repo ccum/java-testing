062e60671897fd8f6519cb336be409fa6da0c377
Cesar Cueva <cesar.cueva@bbva.com>
2020-02-03 23:01:08 -0500

    primer ejemplo con tdd

--- testing-demo/src/main/java/com/cecum/testing/DateUtil.java
+++ testing-demo/src/main/java/com/cecum/testing/DateUtil.java
@@ -5,7 +5,7 @@ package com.cecum.testing;
  */
 public class DateUtil {
 
-    public static boolean isLeapyear(int year) {
+    public static boolean isLeapyear(final int year) {
         if((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0) ){
             return true;
         }
